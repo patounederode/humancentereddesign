7f131478276f7e2c174130082c44cb2702394457
patounederode <153190236+patounederode@users.noreply.github.com>
2026-04-02 11:30:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function nextLine() {
 
   lines[current].classList.add("active");
 }
+
+
+window.onload = function () {
+
+  let current = 0;
+  const lines = document.querySelectorAll(".line");
+
+  // automatische transcript
+  setInterval(() => {
+    lines[current].classList.remove("active");
+    current++;
+
+    if (current >= lines.length) {
+      current = 0;
+    }
+
+    lines[current].classList.add("active");
+  }, 2000);
+
+  // tekstgrootte aanpassen
+  const slider = document.getElementById("fontSize");
+  const transcript = document.getElementById("transcript");
+
+  slider.addEventListener("input", function(e) {
+    transcript.style.fontSize = e.target.value + "px";
+  });
+
+};
+
